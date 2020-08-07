5b581f4b26b6c40d8dec5ea9bf626b57b21f1d28
ktscates <cates2506@gmail.com>
2020-08-07 20:41:51 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('shaun')
+
+  const [person, setPerson] = useState({name: 'mario', age: 40})
+
+  const clickHandler = () => {
+    setName('Cates')
+    setPerson({name: 'yoshi', age: 50})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and he's {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler}/>
+      </View>
+      
     </View>
   );
 }
@@ -14,8 +27,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  buttonContainer: {
+    marginTop: 20
+  }
+
 });
